002533f7b13f250b0986fdad4aac8829afa54a55
sudoboneman <boneman7908@gmail.com>
2026-07-09 01:33:38 +0530

    fix: renamed app.py to main.py to fix python shadowing error

--- main.py
+++ main.py
@@ -40,4 +40,4 @@ with gr.Blocks() as demo:
 app = gr.mount_gradio_app(fastapi_app, demo, path="/")
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=7860)
+    uvicorn.run("main:app", host="0.0.0.0", port=7860)
